07225d66d26a255457aa2380b88a13d782f3bdff
Florian Gramsch <florigram@gmail.com>
2024-08-30 12:40:55 +0200

    happy birthday

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div
+    class="h-screen w-screen bg-pink-400 text-blue-950 flex flex-col justify-center items-center space-y-2"
+  >
+    <div class="text-3xl font-bold">Happy Birthday!</div>
+    <div>Hier könnte deine Seite sein!</div>
   </div>
 </template>
