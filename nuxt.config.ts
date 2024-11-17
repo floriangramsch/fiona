a7a2b751f3f636d4492f9742e0066ca6c4e5d9ca
Florian Gramsch <florigram@gmail.com>
2024-11-17 19:24:24 +0100

    manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,23 @@ export default defineNuxtConfig({
       meta: [
         { name: "theme-color", content: "#F5EBE0" },
         { name: "description", content: "Planer für Fiona" },
+        { name: "apple-mobile-web-app-title", content: "Journal" },
       ],
       link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon-96x96.png",
+          sizes: "96x96",
+        },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        { rel: "shortcut icon", href: "/favicon.ico" },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
         {
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Great+Vibes&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap",
